9f6a9daad606c40769f56d29780b28b6482e9346
David Wickes <dave.wickes@gmail.com>
2014-09-29 16:23:16 +0100

    Passes the test for editing

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -42,8 +42,8 @@ describe 'editing restaurants' do
   it "allows a user to edit a restaurant" do
     visit '/restaurants'
     click_link 'Edit KFC'
-    fill_in 'Name', with: 'Kentucky Fried Chicken'
-    click 'Update Restaurant'
+    fill_in 'Restaurant name', with: 'Kentucky Fried Chicken'
+    click_button 'Update Restaurant'
     expect(page).to have_content 'Kentucky Fried Chicken'
     expect(current_path).to eq '/restaurants'
   end
