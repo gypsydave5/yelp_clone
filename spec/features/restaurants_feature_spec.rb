bbe0f05f3c74137bb3a2985944df43091b87b398
David Wickes <dave.wickes@gmail.com>
2014-09-29 16:09:42 +0100

    Adds test for editing a restaurant record

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -33,3 +33,19 @@ describe 'restaurants' do
 
   end
 end
+
+describe 'editing restaurants' do
+  before do
+    Restaurant.create(name: 'KFC')
+  end
+
+  it "allows a user to edit a restaurant" do
+    visit '/restaurants'
+    click_link 'Edit KFC'
+    fill_in 'Name', with: 'Kentucky Fried Chicken'
+    click 'Update Restaurant'
+    expect(page).to have_content 'Kentucky Fried Chicken'
+    expect(current_path).to eq '/restaurants'
+  end
+end
+
