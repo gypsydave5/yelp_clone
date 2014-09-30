5f562a8ff765b1db05e317b3be3e3fe36e9a5d01
David Wickes <dave.wickes@gmail.com>
2014-09-30 12:07:00 +0100

    Rating and comments now displaying for review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,7 +2,13 @@ class ReviewsController < ApplicationController
 
   def new
     @restaurant = Restaurant.find(params[:restaurant_id])
-    @review =  Review.new
+    @review = Review.new
+  end
+
+  def create
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @restaurant.reviews.create(params[:review].permit(:comments, :rating))
+    redirect_to restaurants_path
   end
 
 end
