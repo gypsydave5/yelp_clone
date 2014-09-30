b8a111ad156fe5279bd4119392142b831bc88620
David Wickes <dave.wickes@gmail.com>
2014-09-30 17:19:55 +0100

    Tests and passses for values below 0

--- spec/models/reviews_spec.rb
+++ spec/models/reviews_spec.rb
@@ -8,4 +8,9 @@ RSpec.describe Review, :type => :model do
     review = Review.new(rating: 20, comments: "I'm evil! EVIL!", restaurant_id: restaurant.id)
     expect(review).to have(1).error_on(:rating)
   end
+
+  it 'cannot accept a review with a rating less than zero' do
+    review = Review.new(rating: -5, comments: "Mwahahahaaaa!", restaurant_id: restaurant.id)
+    expect(review).to have(1).error_on(:rating)
+  end
 end
