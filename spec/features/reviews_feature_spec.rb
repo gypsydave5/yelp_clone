38cb1f94df77b74a93c45c1fd3cba3a75bbe29e4
David Wickes <dave.wickes@gmail.com>
2014-10-01 13:05:21 +0100

    Passes feature test for average rating. Slightly refactors the test to be more targeted on the page content.

--- spec/features/reviews_feature_spec.rb
+++ spec/features/reviews_feature_spec.rb
@@ -32,7 +32,7 @@ describe 'reviewing things' do
   it 'displays an average rating for all reviews' do
     leave_review("KFC", "So so", "3")
     leave_review("KFC", "Great", "5")
-    expect(page).to have_content("Average rating: 4")
+    expect(page.find('.average_rating')).to have_content("Average rating: 4")
   end
 
   def leave_review(restaurant, comments, rating)
