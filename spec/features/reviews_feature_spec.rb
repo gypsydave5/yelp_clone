e79d9db1087ff1aaa6e063c990d0343e29edafa6
David Wickes <dave.wickes@gmail.com>
2014-10-03 23:45:40 +0100

    working on images

--- spec/features/reviews_feature_spec.rb
+++ spec/features/reviews_feature_spec.rb
@@ -5,18 +5,20 @@ describe 'reviewing things' do
 
   before do
     Restaurant.create(name: 'KFC')
+    sign_up("dave@dave.com", "12345678")
   end
 
   let(:kfc) {Restaurant.find_by(name: "KFC")}
+  let(:user) {User.find_by(email: "dave@dave.com")}
 
   context "when logged in" do
 
     before do
-      sign_up("dave@dave.com", "12345678")
-      let(:user) {User.find_by(email: "dave@dave.com")}
+      sign_in("dave@dave.com", "12345678")
     end
 
     context "creating reviews" do
+
       it 'allows users to leave reviews using the form which appears alongside restaurants' do
         leave_review("KFC", "It's alright", 3)
         expect(current_path).to eq '/restaurants'
@@ -26,23 +28,26 @@ describe 'reviewing things' do
 
     end
 
-    context 'changing reviews' do
+  end
 
-      before do
-        leave_review("KFC","Love KFC", 5)
-      end
+  context 'changing reviews' do
 
-      it 'allows you to edit reviews you have made' do
-        visit '/restaurants'
-        expect(page.find('.review')).to have_content "Love KFC"
-        click_link 'Edit review'
-        fill_in "Comments", with: "It's alright"
-        select '3', from: 'Rating'
-        click_button 'Submit Review'
-        expect(page.find('.review')).to have_content "It's alright"
-        expect(page.find('.rating')).to have_content "3"
-      end
+    before do
+      leave_review("KFC","Love KFC", 5)
     end
+
+    it 'allows you to edit reviews you have made' do
+      visit '/restaurants'
+      expect(page.find('.review')).to have_content "Love KFC"
+      click_link 'Edit review'
+      fill_in "Comments", with: "It's alright"
+      select '3', from: 'Rating'
+      click_button 'Submit Review'
+      expect(page.find('.review')).to have_content "It's alright"
+      expect(page.find('.rating')).to have_content "3"
+    end
+
+  end
 
   it 'displays an average rating for all reviews' do
     leave_review("KFC", "So so", "1")
@@ -51,13 +56,16 @@ describe 'reviewing things' do
   end
 
   context "showing time since creation" do
+
     before do
       Review.create(comments: "Hi", rating: 3, restaurant_id: kfc.id, created_at: (Time.now - 120))
     end
+
     it 'displays the time that has passed since the review was posted' do
       visit '/restaurants'
       expect(page.find('.time_since')).to have_content("2 minutes")
     end
+
   end
 
 end
